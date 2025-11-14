8d651441e47e7b919f706b43e92344860b35f2ca
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 11:48:39 -0300

    Update helpers.py

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,1 +1,24 @@
+import random
+import string
+import os
 
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+
+def random_string(n=8):
+return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+
+
+
+
+def random_user():
+username = f'user_{random_string(8)}'
+password = f'P@ss{random_string(8)}'
+return {
+'username': username,
+'password': password
+}
